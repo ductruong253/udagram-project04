12fff5c3d52deffc8e632c1517082df19d33e4c5
ductruong253 <ddtruong94@gmail.com>
2022-08-12 22:28:50 +0700

    working on 0Auth

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-v1l0wntb.us.auth0.com',            // Auth0 domain
+  clientId: 'sxS02wJLWE0GVahF8cRW6dT0dxwaP5LB',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
